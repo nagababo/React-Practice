f94d98e3b8203e5c6ec8d884f6dc34c805f6eb0a
Nagababu Kantamaneni <nagababuk@hotmail.com>
2025-09-22 15:27:56 +0530

    First session

---
+++ app.js
@@ -0,0 +1,25 @@
+
+
+
+{/* <div id="parent">
+    <div id="child1">
+        <h1></h1>
+        <h2> </h2>
+
+    </div>
+    <div id="child2">
+        <h1></h1>
+        <h2> </h2>
+    </div>
+</div> */}
+//const heading = React.createElement("h1", { id: "heading" }, "I am from React");
+const parent = React.createElement("div",{id:"parent"},
+    [React.createElement("div",{id:"child1"},
+    [React.createElement("h1",{},"heading1"),React.createElement("h2",{},"heading2")]),
+    React.createElement("div",{id:"child2"},
+    [React.createElement("h1",{},"heading1"),React.createElement("h2",{},"heading2")])]
+)
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(parent)
+
+
