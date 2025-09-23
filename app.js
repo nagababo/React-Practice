6ee2a7a2328b4e3b66406115f26270c851c98a7f
Nagababu Kantamaneni <nagababuk@hotmail.com>
2025-09-23 13:57:44 +0530

    feat: add React and ReactDOM dependencies, update App.js to use React elements, and enhance README

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 
 
 
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 {/* <div id="parent">
     <div id="child1">
         <h1></h1>
@@ -15,11 +17,12 @@
 //const heading = React.createElement("h1", { id: "heading" }, "I am from React");
 const parent = React.createElement("div",{id:"parent"},
     [React.createElement("div",{id:"child1"},
-    [React.createElement("h1",{},"heading1"),React.createElement("h2",{},"heading2")]),
+    [React.createElement("h1",{},"heading1"),React.createElement("h2",{},"This is React Pracice ")]),
     React.createElement("div",{id:"child2"},
     [React.createElement("h1",{},"heading1"),React.createElement("h2",{},"heading2")])]
 )
 const root = ReactDOM.createRoot(document.getElementById("root"))
+
 root.render(parent)
 
 
--- readme.md
+++ readme.md
@@ -1,1 +1,13 @@
-React Practice
+React Practice 🚀🚀
+
+#Parcel
+- Dev Build
+- Local Server
+- HMR - Hot Module Replacement
+- File watching algorithm - written in c++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compressing
+- 
