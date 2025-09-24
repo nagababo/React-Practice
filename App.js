0f3948172b53487c7bd392c5ac0d180fca95d861
nagababo <nagababo@gamil.com>
2025-09-24 20:53:58 +0530

    feat: implement initial React setup with App.js and update index.html for rendering

---
+++ App.js
@@ -0,0 +1,16 @@
+
+
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const heading = (<h1 className='head'
+     tabIndex="1">Nameste React</h1>
+)
+ 
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(heading)
+
+
