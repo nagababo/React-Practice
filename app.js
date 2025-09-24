d8570cf5bc8afd4050f70808905fd809162b1a3f
nagababo <nagababo@gamil.com>
2025-09-24 20:20:09 +0530

    refactor: update rendering logic in app.js to use jsxHeading and correct script path in index.html

--- app.js
+++ app.js
@@ -3,26 +3,38 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-{/* <div id="parent">
-    <div id="child1">
-        <h1></h1>
-        <h2> </h2>
 
-    </div>
-    <div id="child2">
-        <h1></h1>
-        <h2> </h2>
-    </div>
-</div> */}
-//const heading = React.createElement("h1", { id: "heading" }, "I am from React");
-const parent = React.createElement("div",{id:"parent"},
-    [React.createElement("div",{id:"child1"},
-    [React.createElement("h1",{},"heading1"),React.createElement("h2",{},"This is React Pracice ")]),
-    React.createElement("div",{id:"child2"},
-    [React.createElement("h1",{},"heading1"),React.createElement("h2",{},"heading2")])]
+//React.createElement => ReactElement - (js object)=>HTMLElement(render)
+// const heading = React.createElement(
+//     "h1",
+//     {},
+//     "Namaste React"
+// );
+// console.log(heading,"ReactElement");
+//jsx=>React.createElement=>ReactElement(js object)=>HTMLElement(render)
+//single line jsx not brackets requiered
+//const jsxHeading = <h1 className='head' tabIndex="1">Nameste React</h1>
+//multi line jsx bracket are mandatory
+const jsxHeading = (<h1 className='head'
+     tabIndex="1">Nameste React</h1>
 )
+ console.log(jsxHeading,"jsxheading") 
+ {
+    {
+        {
+
+        }
+    }
+ }
+
+// const parent = React.createElement("div",{id:"parent"},
+//     [React.createElement("div",{id:"child1"},
+//     [React.createElement("h1",{},"heading1"),React.createElement("h2",{},"This is React Pracice ")]),
+//     React.createElement("div",{id:"child2"},
+//     [React.createElement("h1",{},"heading1"),React.createElement("h2",{},"heading2")])]
+// )
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(parent)
+root.render(jsxHeading)
 
 
