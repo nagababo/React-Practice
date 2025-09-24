2d0cb8e0e2fafe3d8bc55d50a36e5f6ae07c26c2
nagababo <nagababo@gamil.com>
2025-09-25 03:42:51 +0530

    feat: add element to HeadingComponent and simplify package.json scripts

--- App.js
+++ App.js
@@ -12,8 +12,10 @@ const Title = ()=>(
 //      return <h1>i am from HeadingComponent</h1>
 //  }
 
+const elem = <span>hi</span>
 const HeadingComponent = () => (
   <div id="container">
+    {elem}
     <Title/>
     <h1 id="heading">I am from functional Heading Component</h1>
   </div>
