28142a2a49c7025983eb7fb9e35753e025811805
nagababo <nagababo@gamil.com>
2025-09-28 03:58:46 +0530

    feat: implement filtering functionality for top-rated restaurants in Body component

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,24 @@
+import {useState} from 'react'
 import RestaurantCard from "./RestaurantCard"
 import restaurants  from "../utils/mockData"
+
 const Body = () => {
+
+const [listOfRestaurant, setListOfRestaurant]= useState(restaurants)
+
+  const handleFilter =()=>{
+    const filteredRestaurants = restaurants.filter(res=>res.info.avgRating >4.2)
+    setListOfRestaurant(filteredRestaurants)
+  }
 
   return (
     <div className="body">
       <div className="search">Search</div>
+       <div className="filter">
+        <button className="filter-btn" onClick={handleFilter}>Top Rated Restaurant</button>
+       </div>
       <div className="res-container">
-        {restaurants.map((restaurant) => (
+        {listOfRestaurant.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant?.info} />
         ))}
       </div>
