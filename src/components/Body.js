94d0007c89852b181cdce9a124803962d46b43b3
nagababo <nagababo@gamil.com>
2025-09-29 06:46:45 +0530

    feat: fetch restaurant data from API and update state management in Body component

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,40 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import RestaurantCard from "./RestaurantCard"
 import restaurants  from "../utils/mockData"
 
-const Body = () => {
+const Body = () => { 
 
-const [listOfRestaurant, setListOfRestaurant]= useState(restaurants)
+const [listOfRestaurant, setListOfRestaurant]= useState([])
 
   const handleFilter =()=>{
     const filteredRestaurants = restaurants.filter(res=>res.info.avgRating >4.2)
     setListOfRestaurant(filteredRestaurants)
   }
 
+  useEffect(()=>{
+
+    getData()
+  },[])
+
+  const getData = async()=>{
+    const data = await fetch("https://swiggy-api-4c740.web.app/swiggy-api.json")
+    const json = await data.json()
+  // console.log(json.data.cards[1].card.gridElements.infoWithStyle.restaurants[0].info, "json")
+
+  // const restaurants = json.data.cards[1].card.gridElements.infoWithStyle.restaurants;
+
+//console.log(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants,"json");
+setListOfRestaurant(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants)
+
+// → Array of all restaurant objects
+
+
+//console.log(json.data.gridElements.infoWithStyle.restaurants[0].info)
+
+
+
+
+  }
   return (
     <div className="body">
       <div className="search">Search</div>
