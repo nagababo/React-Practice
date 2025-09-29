aa74b1fe4b458f5dbbd2710774613b9a14ab1b03
nagababo <nagababo@gamil.com>
2025-09-29 10:38:54 +0530

    feat: add Shammer component for loading placeholder and update Body component to display it during data fetching

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,52 +1,69 @@
-import {useEffect, useState} from 'react'
-import RestaurantCard from "./RestaurantCard"
-import restaurants  from "../utils/mockData"
+import { useEffect, useState } from "react";
+import RestaurantCard from "./RestaurantCard";
+import restaurants from "../utils/mockData";
+import Shammer from "./Shammer"
 
-const Body = () => { 
+const Body = () => {
+  const [listOfRestaurant, setListOfRestaurant] = useState([]);
 
-const [listOfRestaurant, setListOfRestaurant]= useState([])
+  const handleFilter = () => {
+    const filteredRestaurants = restaurants.filter(
+      (res) => res.info.avgRating > 4.2
+    );
+    setListOfRestaurant(filteredRestaurants);
+  };
 
-  const handleFilter =()=>{
-    const filteredRestaurants = restaurants.filter(res=>res.info.avgRating >4.2)
-    setListOfRestaurant(filteredRestaurants)
-  }
+  useEffect(() => {
+    setTimeout(() => {
+      getData();
+    }, 5000);
+  }, []);
 
-  useEffect(()=>{
+  const getData = async () => {
+    const data = await fetch(
+      "https://swiggy-api-4c740.web.app/swiggy-api.json"
+    );
+    const json = await data.json();
+    // console.log(json.data.cards[1].card.gridElements.infoWithStyle.restaurants[0].info, "json")
 
-    getData()
-  },[])
+    // const restaurants = json.data.cards[1].card.gridElements.infoWithStyle.restaurants;
 
-  const getData = async()=>{
-    const data = await fetch("https://swiggy-api-4c740.web.app/swiggy-api.json")
-    const json = await data.json()
-  // console.log(json.data.cards[1].card.gridElements.infoWithStyle.restaurants[0].info, "json")
+    //console.log(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants,"json");
+    setListOfRestaurant(
+      json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+    );
 
-  // const restaurants = json.data.cards[1].card.gridElements.infoWithStyle.restaurants;
+    // → Array of all restaurant objects
 
-//console.log(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants,"json");
-setListOfRestaurant(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants)
+    //console.log(json.data.gridElements.infoWithStyle.restaurants[0].info)
+  };
 
-// → Array of all restaurant objects
-
-
-//console.log(json.data.gridElements.infoWithStyle.restaurants[0].info)
-
-
-
-
-  }
   return (
-    <div className="body">
-      <div className="search">Search</div>
-       <div className="filter">
-        <button className="filter-btn" onClick={handleFilter}>Top Rated Restaurant</button>
-       </div>
-      <div className="res-container">
-        {listOfRestaurant.map((restaurant) => (
-          <RestaurantCard key={restaurant.info.id} resData={restaurant?.info} />
-        ))}
-      </div>
-    </div>
-  )
-}
-export default Body
+    <>
+      {listOfRestaurant.length === 0 ? (
+        <>
+          <Shammer />
+          
+        </>
+      ) : (
+        <div className="body">
+          <div className="search">Search</div>
+          <div className="filter">
+            <button className="filter-btn" onClick={handleFilter}>
+              Top Rated Restaurant
+            </button>
+          </div>
+          <div className="res-container">
+            {listOfRestaurant.map((restaurant) => (
+              <RestaurantCard
+                key={restaurant.info.id}
+                resData={restaurant?.info}
+              />
+            ))}
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+export default Body;
---
+++ src/components/Shammer.js
@@ -0,0 +1,32 @@
+const Shammer = ()=>{
+return(
+    <>
+    <div className="shammerContainer">
+        <div className="shammer">
+
+        </div>
+        <div className="shammer">
+
+        </div>
+        <div className="shammer">
+
+        </div>
+        <div className="shammer">
+
+        </div>
+        <div className="shammer">
+
+        </div>
+        <div className="shammer">
+
+        </div>
+        <div className="shammer">
+
+        </div>
+
+    </div>
+    </>
+)
+}
+
+export default Shammer
