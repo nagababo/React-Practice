dd98459f1d678579557f564fe42184162f9b2af9
Nagababu Kantamaneni <nagababuk@hotmail.com>
2025-10-07 04:20:10 +0530

    feat: add RestaurantCategory and ItemList components for structured menu display in RestaurantMenu

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -3,27 +3,47 @@ import { useParams } from "react-router";
 import { RES_DET_URL } from "../utils/constants";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
 import Shimmer from "./Shimmer"
+import RestaurantCategory from "./RestaurantCategory";
 
 const RestaurantMenu = () => {
+const [showIndex, setShowIndex] = useState(null)
+const [showItems, setShowItems] = useState(true)
+
   const { resId } = useParams();
   const resInfo = useRestaurantMenu(resId)
 
   if (resInfo === null) return <Shimmer />
 
-  console.log(resInfo,"resInfo")
+ // console.log(resInfo, "resInfo")
 
   const { name, cuisines, costForTwoMessage } = resInfo.cards[2].card.card.info;
- // const { itemCards } = resInfo?.cards[2]?.groupedCard?.cardGroup?.REGULAR?.cards[1]?.card?.card;
+  const { itemCards } = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+
+ // console.log(resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards, "itemCards")
+
+  const categories = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter((c) => c.card?.card?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory")
 
   return (
     <>
-      <div className="menu">
-        <h1>{name}</h1>
-        <p>{cuisines.join(", ")} - {costForTwoMessage}</p>
-        <h2>Menu</h2>
+      <div className="text-center">
+        <h1 className="font-bold my-6 text-2xl">{name}</h1>
+        <p className="font-bold text-lg">{cuisines.join(", ")} - {costForTwoMessage}</p>
+
+      { categories.map((category,index)=>(
+
+        <RestaurantCategory key= {category?.card?.card?.title} data = {category?.card?.card} showItems={index===showIndex && true} 
+        setShowIndex={()=>setShowIndex(index)} />
+      ))}
+
+
+        {/* <h2>Menu</h2>
         <ul>
-          <li></li>
-        </ul>
+          {itemCards.map((item) => (
+            <li key={item.card.info.id}>
+              {item.card.info.name} - {" Rs. "} {item.card.info.price / 100 || item.card.info.defaultPrice / 100}
+            </li>
+          ))}
+        </ul> */}
       </div>
     </>
   );
